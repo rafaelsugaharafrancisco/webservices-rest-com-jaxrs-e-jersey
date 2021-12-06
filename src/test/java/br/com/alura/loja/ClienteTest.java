da95ee2dcde4493ced30d98cc02828d89181b3a5
Rafael <rafa.s.francisco@gmail.com>
2021-12-05 21:39:06 -0300

    teste de conexão com o servidor

---
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -0,0 +1,24 @@
+package br.com.alura.loja;
+
+import static org.junit.Assert.assertTrue;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.junit.Test;
+
+
+public class ClienteTest {
+	
+	@Test
+	public void TestaConexaoComServidor() {
+		Client client = ClientBuilder.newClient();
+		
+		WebTarget target = client.target("http://www.mocky.io");
+		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
+		
+		assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));
+	}
+
+}
