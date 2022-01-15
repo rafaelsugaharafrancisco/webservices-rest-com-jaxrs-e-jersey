53197142752f1e86c7eb06d51cf534d65251cbb7
Rafael <rafa.s.francisco@gmail.com>
2022-01-14 23:20:52 -0300

    implementando o metodo POST

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -1,10 +1,14 @@
 package br.com.alura.loja;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
@@ -15,6 +19,7 @@ import org.junit.Test;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 
 public class ClienteTest {
@@ -32,7 +37,7 @@ public class ClienteTest {
 	}
 	
 	@Test
-	public void TestaConexaoComServidor() {
+	public void testaConexaoComServidor() {
 		Client client = ClientBuilder.newClient();
 		
 		WebTarget target = client.target("http://www.mocky.io");
@@ -50,6 +55,22 @@ public class ClienteTest {
 		Carrinho carrinho = (Carrinho)new XStream().fromXML(conteudo);
 		
 		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+	}
+	
+	@Test
+	public void testaAdicionaCarrinho() {
+		Client client = ClientBuilder.newClient();
+		
+		WebTarget target = client.target("http://localhost:8080");
+		Carrinho carrinho = new Carrinho().adiciona(new Produto(314L, "iphone 9", 3300.00, 1));
+		carrinho.para("Rua Odemis, 292", "Sao Paulo");
+		
+		Entity<String> entity = Entity.entity(carrinho.toXML(), MediaType.APPLICATION_XML);
+		
+		Response response = target.path("/carrinhos").request().post(entity);
+		
+		assertEquals("<status> Carrinho criado com sucesso </status>", response.readEntity(String.class));
+		
 	}
 
 }
